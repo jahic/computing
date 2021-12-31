d34126565a3107f2f2b31f1f4021c6e3e82cb8aa
Jasmin Jahic <jasmin.jahic@gmail.com>
2021-12-31 13:42:22 +0000

    Added exampels for CUDA.

---
+++ CUDA_addVector/addVectorWithoutCuda.c
@@ -0,0 +1,43 @@
+/**
+*	@Author: https://cuda-tutorial.readthedocs.io/en/latest/tutorials/tutorial01/#tutorial-01-say-hello-to-cuda
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+#define N 600000000
+#define MAX_ERR 1e-6
+
+void vector_add(float *out, float *a, float *b, int n) {
+    for(int i = 0; i < n; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+
+int main(){
+    float *a, *b, *out; 
+
+    // Allocate memory
+    a   = (float*)malloc(sizeof(float) * N);
+    b   = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+
+    // Initialize array
+    for(int i = 0; i < N; i++){
+        a[i] = 1.0f;
+        b[i] = 2.0f;
+    }
+
+    // Main function
+    vector_add(out, a, b, N);
+
+    // Verification
+    for(int i = 0; i < N; i++){
+        assert(fabs(out[i] - a[i] - b[i]) < MAX_ERR);
+    }
+
+    printf("out[0] = %f\n", out[0]);
+    printf("PASSED\n");
+}
